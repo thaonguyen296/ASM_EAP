62a3400329e293e5f0997507006c81e471bb27d6
Thao <thaonttth1807035@fpr.edu.vn>
2020-01-06 18:09:41 +0700

    day bai assignment mon EAP

---
+++ EAP_ASM_NTT/Models/MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EAP_ASM_NTT.Models
+{
+    public class MyDbContext : IdentityDbContext<Account>
+    {
+        public MyDbContext() : base("name=MyDbContext")
+        {
+        }
+
+        public static MyDbContext Create()
+        {
+            return new MyDbContext();
+        }
+
+        public System.Data.Entity.DbSet<EAP_ASM_NTT.Models.Market> Markets { get; set; }
+
+        public System.Data.Entity.DbSet<EAP_ASM_NTT.Models.Coin> Coins { get; set; }
+    }
+}
---
+++ EAP_ASM_NTT/Startup.cs
@@ -0,0 +1,28 @@
+﻿using EAP_ASM_NTT.App_Start;
+using EAP_ASM_NTT.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication11.Startup))]
+namespace WebApplication11
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<MyDbContext>(MyDbContext.Create);
+            app.CreatePerOwinContext<MyUserManager>(MyUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Accounts/Login")
+            });
+        }
+    }
+}
